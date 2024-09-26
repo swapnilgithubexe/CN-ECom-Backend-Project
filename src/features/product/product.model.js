575b156cbd77e34feeef928a69b12eee4748696f
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-26 22:51:30 +0530

    updates

---
+++ src/features/product/product.model.js
@@ -0,0 +1,37 @@
+export default class ProductModel {
+  constructor(id, name, desc, price, imageUrl, category, sizes) {
+    this.id = id;
+    this.name = name;
+    this.desc = desc;
+    this.imageUrl = imageUrl;
+    this.category = category;
+    this.price = price;
+    this.sizes = sizes;
+  }
+}
+
+
+var products = [
+  new ProductModel(
+    1,
+    'Product 1',
+    'Description for Product 1',
+    19.99,
+    'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg',
+    "Category1"
+  ),
+  new ProductModel(
+    2,
+    'Product 2',
+    'Description for Product 2',
+    29.99,
+    'https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg',
+  ),
+  new ProductModel(
+    3,
+    'Product 3',
+    'Description for Product 3',
+    39.99,
+    'https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg',
+  ),
+]
