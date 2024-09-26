b43e6ff0c415b7e75612a83cfd46996fefd12206
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-26 15:14:05 +0530

    updates

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 import express from "express";
+
+const server = express();
+
+
+server.get("/", (req, res) => {
+  res.send("Welcome Guys!")
+})
+server.listen(4000, () => {
+  console.log("Server is running on PORT number 4000");
+
+})
