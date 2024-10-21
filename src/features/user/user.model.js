d37bfea7f07a49a987802114a439827792cae5bb
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-10-21 22:26:32 +0530

    updates

--- src/features/user/user.model.js
+++ src/features/user/user.model.js
@@ -1,14 +1,17 @@
-export class UserModel {
-  constructor(name, email, password, type) {
+export default class UserModel {
+  constructor(name, email, password, type, id) {
     this.name = name;
     this.email = email,
       this.password = password;
     this.type = type;
+    this.id = id;
   }
 
   static SignUp(name, email, password, type) {
     const newUser = new UserModel(name, email, password, type)
+    newUser.id = users.length() + 1;
     users.push(newUser);
+    return newUser;
   }
 
   static SignIn(email, password) {
@@ -18,6 +21,7 @@ export class UserModel {
 }
 
 var users = [{
+  id: 1,
   name: "SellerUser",
   email: "Seller@gmail.com",
   password: "PasswordForSelling",
---
+++ src/features/user/user.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { UserController } from "./user.controller.js";
+
+const router = express.Router();
+const userController = new UserController();
+
+router.post("/signin", userController.signIn);
+router.post("/signup", userController.signUp);
