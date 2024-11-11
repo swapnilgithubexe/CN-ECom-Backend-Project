45e7289a9bd140312c19a7615cfb5548c047b05d
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-11-12 00:47:24 +0530

    updates

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 const server = express();
+import swagger from "swagger-ui-express";
+import apiDocs from "./swagger.json" assert {type: 'json'};
 
 server.use(bodyParser.json());
 
@@ -10,6 +12,7 @@ import userRouter from "./src/features/user/user.routes.js";
 import jwtAuth from "./src/middleware/jwt.middleware.js";
 import cartRouter from "./src/features/cart/cart.routes.js";
 
+
 // Mount productRouter on '/products'
 server.use("/products", jwtAuth, productRouter);
 server.use("/api/users", userRouter);
@@ -18,6 +21,8 @@ server.use("/api/cartItems", jwtAuth, cartRouter)
 server.get("/", (req, res) => {
   res.send("Welcome Guys!");
 });
+
+server.use("/api-docs", swagger.serve, swagger.setup(apiDocs))
 
 server.listen(4000, () => {
   console.log("Server is running on PORT number 4000");
