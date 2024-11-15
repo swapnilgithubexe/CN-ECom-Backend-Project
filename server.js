f8e07e1e5e43e5bb7b021ad1314a2da53a9ee3a2
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-11-15 16:13:55 +0530

    updates

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 const server = express();
 import swagger from "swagger-ui-express";
-import apiDocs from "./swagger.json" assert {type: 'json'};
-
+import apiDocs from "./swagger.json" assert { type: "json" };
 
 //CORS policy config
 server.use((req, res, next) => {
-  res.header("Acess-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Acess-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   //return ok for pre flight request
   if (req.method == "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
-})
+});
 
 server.use(bodyParser.json());
 
@@ -25,22 +24,21 @@ import userRouter from "./src/features/user/user.routes.js";
 import jwtAuth from "./src/middleware/jwt.middleware.js";
 import cartRouter from "./src/features/cart/cart.routes.js";
 
-
 // Mount productRouter on '/products'
 server.use("/products", jwtAuth, productRouter);
 server.use("/api/users", userRouter);
-server.use("/api/cartItems", jwtAuth, cartRouter)
+server.use("/api/cartItems", jwtAuth, cartRouter);
 
 server.get("/", (req, res) => {
   res.send("Welcome Guys!");
 });
 
-server.use("/api-docs", swagger.serve, swagger.setup(apiDocs))
+server.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
 
 //For routes which doesn't exist
 server.use((req, res) => {
-  res.status(404).send("<h1>API not found!</h1>")
-})
+  res.status(404).send("<h1>API not found!</h1>");
+});
 
 server.listen(4000, () => {
   console.log("Server is running on PORT number 4000");
