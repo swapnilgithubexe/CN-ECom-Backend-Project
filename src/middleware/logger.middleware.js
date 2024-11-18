fbc8026fadf78a1b62260784b84dc50d1838e46c
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-11-18 10:31:47 +0530

    updates

--- src/middleware/logger.middleware.js
+++ src/middleware/logger.middleware.js
@@ -1,16 +1,25 @@
 import fs from "fs";
+import winston from "winston";
 
 const fsPromise = fs.promises;
 
-const log = (logData) => {
-  try {
-    logData = `\n ${new Date().toString()} - ${logData}`
-    fsPromise.appendFile("log.txt", logData);
-  } catch (error) {
-    console.log(error);
+// const log = (logData) => {
+//   try {
+//     logData = `\n ${new Date().toString()} - ${logData}`
+//     fsPromise.appendFile("log.txt", logData);
+//   } catch (error) {
+//     console.log(error);
 
-  }
-}
+//   }
+// }
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: 'logs.txt' })]
+})
+
+
 
 const loggerMiddleware = async (req, res, next) => {
   //log request body
