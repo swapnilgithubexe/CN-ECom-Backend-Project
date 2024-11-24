b8f3f7d3889ba59c1bf2ca0f6760b374b73fbc25
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-11-24 13:56:37 +0530

    Auto-commit on save

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -20,7 +20,7 @@ export default class ProductController {
   rateProduct(req, res) {
     const userID = req.query.userID;
     const productID = req.query.productID;
-    const rating = req.query.rating;
+    const rating = req.querys.rating;
     try {
       ProductModel.rateProduct(userID, productID, rating)
     } catch (error) {
