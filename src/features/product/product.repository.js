e722cb5c733583212660bd83f7241089f8fb3a65
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-12-27 14:42:49 +0530

    updates

--- src/features/product/product.repository.js
+++ src/features/product/product.repository.js
@@ -2,11 +2,14 @@ import { getDB } from "../../config/mongodb.js";
 import { ApplicationError } from "../../error/applicationError.js";
 
 class ProductRepository {
+  constructor() {
+    this.collection = "products";
+  }
   async add(newProduct) {
     try {
       const db = getDB();
 
-      const collection = db.collection("products");
+      const collection = db.collection(this.collection);
 
       return await collection.insertOne({ newProduct });
     } catch (error) {
