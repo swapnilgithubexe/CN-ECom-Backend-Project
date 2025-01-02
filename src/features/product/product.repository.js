f1a32b54e0a49d7d02a72097670c7bdfff1b5d4a
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-01-03 00:15:32 +0530

    updates

--- src/features/product/product.repository.js
+++ src/features/product/product.repository.js
@@ -74,12 +74,59 @@ export default class ProductRepository {
       const db = getDB();
       const collection = db.collection(this.collection);
 
-      await collection.updateOne({ _id: new ObjectId(productID) }, {
-        $push: { ratings: { userID: new ObjectId(userID), rating } }
-      });
+      const productObjectID = new ObjectId(productID);
+      const userObjectID = new ObjectId(userID);
+      //Finding the product
+      const product = await collection.findOne({ _id: productObjectID });
+
+      //Finding if the same user has any rating beforehand
+      const userRating = product?.ratings?.find(r => r.userID.equals(userObjectID));
+      // ?. is null check to eradicate the undefined typeError
+      //similary we could write if(product && product.ratings)
+      if (userRating) {
+        await collection.updateOne({ _id: productObjectID, "ratings.userID": userObjectID }, {
+          $set: { "ratings.$.rating": rating }
+        })
+      } else {
+        await collection.updateOne({ _id: productObjectID }, {
+          $push: { ratings: { userID: userObjectID, rating } }
+        });
+      }
     } catch (error) {
       console.error(error.message);
       throw new ApplicationError("Something is wrong with the database", 500);
     }
   }
+  // async rate(userID, productID, rating) {
+  //   try {
+  //     const db = getDB();
+  //     const collection = db.collection(this.collection);
+
+  //     const productObjectId = new ObjectId(productID);
+  //     const userObjectId = new ObjectId(userID);
+
+  //     const product = await collection.findOne({
+  //       _id: productObjectId,
+  //       "ratings.userID": userObjectId, // Match the rating with this userID
+  //     });
+
+  //     if (product) {
+  //       await collection.updateOne(
+  //         { _id: productObjectId, "ratings.userID": userObjectId },
+  //         { $set: { "ratings.$.rating": rating } } 
+  //       );
+  //     } else {
+  //       await collection.updateOne(
+  //         { _id: productObjectId },
+  //         {
+  //           $push: { ratings: { userID: userObjectId, rating } },
+  //         }
+  //       );
+  //     }
+  //   } catch (error) {
+  //     console.error(error.message);
+  //     throw new ApplicationError("Something is wrong with the database", 500);
+  //   }
+  // }
+
 }
