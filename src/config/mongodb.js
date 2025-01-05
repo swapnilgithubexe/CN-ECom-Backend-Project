70d9723dbda77f533041981390849f325ad14a87
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-01-06 01:04:58 +0530

    updates

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -8,6 +8,7 @@ export const mongodbconnection = () => {
   MongoClient.connect(process.env.DB_URL).then((clientInstance) => {
     client = clientInstance
     console.log("Database Connected");
+    createIndexes(client.db())
 
   }).catch(err => {
     console.log(err);
@@ -17,6 +18,12 @@ export const mongodbconnection = () => {
 
 export const getDB = () => {
   return client.db();
+}
+
+export const createIndexes = async (db) => {
+  await db.collection("products").createIndex({ price: 1 });
+  await db.collection("products").createIndex({ price: -1, name: 1 });
+  await db.collection("products").createIndex({ desc: "text" });
 }
 
 
